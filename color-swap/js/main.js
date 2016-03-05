63f0e47eeb9adc0b167cbea167cf8c9f8429b49e
Trevor Gau <trevorsg@gmail.com>
2016-03-05 15:38:35 -0500

    Initial commit - color swap, playing around with xjs framework.

---
+++ color-swap/js/main.js
@@ -0,0 +1,31 @@
+(function () {
+    'use strict';
+
+    /**
+     * Hi! Thanks for using XSplit JS Framework :)
+     *
+     * XSplit JS Framework exposes useful methods to work with XSplit without 
+     * fiddling too much with the raw core methods exposed to JavaScript, which 
+     * makes developing plugins for XSplit a lot easier.
+     *
+     * To get started, you just have to require our library:
+     * var xjs = require('xjs');
+     *
+     * You can read more about it at our documentation:
+     * http://xjsframework.github.io/quickstart.html
+     * http://xjsframework.github.io/tutorials.html
+     * http://xjsframework.github.io/api.html
+     */
+    var xjs = require('xjs');
+
+    xjs.ready().then(function () {
+        xjs.Scene.getByIdAsync(5).then(scene => {
+            scene.getSources().then(sources => {
+                if (sources.length) {
+                    let val = sources[0].getCustomName();
+                    sources[0].setCustomName(val + "1");
+                }
+            });
+        });
+    });
+})();
